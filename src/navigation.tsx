b8319f9096f21ecbb52de03511396eb73601f8c4
succcubbus <jannis.kaiser2@gmail.com>
2018-06-14 21:55:15 +0200

    fix lint warnings in navigation.tsx

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,53 +1,47 @@
-import * as React from "react";
-import {Feed} from "./feed";
-import {Overview} from "./overview";
-import {Component, ComponentClass} from "react";
+import * as React from 'react';
+import { Feed } from './feed';
+import { Overview } from './overview';
 
 interface NavItem {
-    title: string;
-    component: ComponentClass;
+  title: string;
+  component: React.ComponentClass;
 }
 
-export class Navigation extends Component {
-    navItems : NavItem[] = [
-        {title: 'Main', component: Feed},
-        {title: 'Shows', component: Overview},
-        {title: 'Subscriptions', component: Overview},
-    ];
+export class Navigation extends React.Component {
+  public navItems: NavItem[] = [
+    { title: 'Main', component: Feed },
+    { title: 'Shows', component: Overview },
+    { title: 'Subscriptions', component: Overview },
+  ];
 
-    state: {activeComponent: ComponentClass} = {activeComponent: this.navItems[0].component};
+  public state: { activeComponent: React.ComponentClass } = {
+    activeComponent: this.navItems[0].component,
+  };
 
-    navigate(item: NavItem) {
-        console.log('navigate to: ', item);
-        this.setState({activeComponent: item.component});
-    }
+  public navigate(item: NavItem) {
+    console.debug('navigate to: ', item);
+    this.setState({ activeComponent: item.component });
+  }
 
-    render() {
-        return (
-            <div className="columns">
-                <nav className="column container is-1">
-                <aside className="menu">
-                    <p className="menu-label">
-                        Navigation
-                    </p>
-                    <ul className="menu-list">
-                        {
-                            this.navItems.map(item => (
-                                <li key={item.title} onClick={() => this.navigate(item)}>
-                                    <a>
-                                    {item.title}
-                                    </a>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </aside>
-                </nav>
-                <div className="column">
-                    {React.createElement(this.state.activeComponent)}
-                </div>
-            </div>
-        );
-    }
+  public render() {
+    return (
+      <div className="columns">
+        <nav className="column container is-1">
+          <aside className="menu">
+            <p className="menu-label">Navigation</p>
+            <ul className="menu-list">
+              {this.navItems.map(item => (
+                <li key={item.title} onClick={() => this.navigate(item)}>
+                  <a>{item.title}</a>
+                </li>
+              ))}
+            </ul>
+          </aside>
+        </nav>
+        <div className="column">
+          {React.createElement(this.state.activeComponent)}
+        </div>
+      </div>
+    );
+  }
 }
-
