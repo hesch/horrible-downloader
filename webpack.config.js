56a6ed0aa5d6299485cef680572372ca02a05655
hesch <github@henri.mails-schmidt.de>
2018-06-14 17:54:43 +0200

    add babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,35 @@
+const path = require('path');
+
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            ['@babel/preset-stage-2', {
+                                decoratorsLegacy: true
+                            }]
+                        ],
+                        plugins: []
+                    }
+                }
+            },
+
+        ]
+    },
     devServer: {
         contentBase: "./",
         proxy: {
             '/feed': {
                 target: 'http://horriblesubs.info/rss.php?res=all',
                 changeOrigin: true,
-                pathRewrite: {'^/feed' : ''},
+                pathRewrite: {'^/feed': ''},
             }
         }
     }
