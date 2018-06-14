6000b910464d0cc890bd03a95bf44787a3ec90ed
succcubbus <jannis.kaiser2@gmail.com>
2018-06-14 23:46:41 +0200

    configure electron webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,49 @@
 const path = require('path');
 
 module.exports = {
-    devtool: "source-map",
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+  devtool: 'source-map',
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              [
+                '@babel/preset-stage-2',
+                {
+                  decoratorsLegacy: true,
+                },
+              ],
+            ],
+            plugins: [],
+          },
+        },
+      },
+      {
+        test: /\.tsx?/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['awesome-typescript-loader'],
+      },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+    ],
+  },
+  devServer: {
+    contentBase: './',
+    proxy: {
+      '/feed': {
+        target: 'http://horriblesubs.info/rss.php?res=all',
+        changeOrigin: true,
+        pathRewrite: { '^/feed': '' },
+      },
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react',
-                            ['@babel/preset-stage-2', {
-                                decoratorsLegacy: true
-                            }]
-                        ],
-                        plugins: []
-                    }
-                }
-            },
-            {
-                test: /\.tsx?/,
-                exclude: /(node_modules|bower_components)/,
-                use: ['awesome-typescript-loader'],
-            },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-    devServer: {
-        contentBase: "./",
-        proxy: {
-            '/feed': {
-                target: 'http://horriblesubs.info/rss.php?res=all',
-                changeOrigin: true,
-                pathRewrite: {'^/feed': ''},
-            }
-        }
-    }
+  },
 };
