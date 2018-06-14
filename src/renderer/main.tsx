4f40020c866a2d303d9adbc0e4e5c375b554836f
Henri Schmidt <henri@reschmi.de>
2018-06-14 20:16:25 +0200

    use tsx extension

---
+++ src/renderer/main.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import './main.scss';
+
+const App = () => (
+    <h1>Hallo</h1>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
