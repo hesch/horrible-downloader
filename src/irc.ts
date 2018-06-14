b3d4653b9c4a3460005d64fc574911d7c6cfff3e
Henri Schmidt <henri@reschmi.de>
2018-06-14 22:30:31 +0200

    fix irc

--- src/irc.ts
+++ src/irc.ts
@@ -1,25 +1,34 @@
 import {getDownloadLocation} from "./shared/reducers/root";
+import {Socket} from "net";
 
 const { ipcMain: ipc } = require('electron');
 
 const fs = require('fs');
+const path = require('path');
 const irc = require('irc');
 const DCC = require('irc-dcc');
 
-module.exports = (store) => {
+interface DccArgs {
+  host: string;
+  port: number;
+  filename: string;
+  length: number;
+}
+
+module.exports = (store : {getState: ()=>{}}) => {
 
     const client = new irc.Client('irc.rizon.net', 'testNick', {autoConnect: false, secure: true, port: 6697});
     const dcc = new DCC(client);
 
-    client.addListener('error', function(message) {
-        console.err('irc-err: ', message);
+    client.addListener('error', function(message: string) {
+        console.log('irc-err: ', message);
     });
 
-    client.on('dcc-send', (from, args, message) => {
+    client.on('dcc-send', (from:string, args: DccArgs, message: string) => {
         console.log('received dcc request for file: ' + args.filename);
-        const ws = fs.createWriteStream(paths.append(getDownloadLocation(store.getState()),args.filename));
+        const ws = fs.createWriteStream(path.join(getDownloadLocation(store.getState()),args.filename));
         dcc.acceptFile(from, args.host, args.port, args.filename,
-            args.length, (err, filename, con) => {
+            args.length, (err: string, filename: string, con: Socket) => {
                 console.log('accepting file');
                 if (err) {
                     console.log(err);
@@ -33,7 +42,7 @@ module.exports = (store) => {
 
     const connected = new Promise(client.connect);
 
-    ipc.on('download', (event, {bot, pack}) => {
+    ipc.on('download', (event: any, {bot, pack}: {bot:string, pack:number}) => {
         connected.then(() => {
             client.say(bot, `xdcc send ${pack}`);
         });
