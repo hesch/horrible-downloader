43763b02693845f4a5c44f744f7da2350fc800c8
hesch <github@henri.mails-schmidt.de>
2018-06-14 18:12:06 +0200

    bootstrap overview component

---
+++ src/overview.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+export default class Overview extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <h1 className="title">Alle Serien</h1>
+                <ul>
+                    <li>Entry 1</li>
+                    <li>Entry 2</li>
+                    <li>Entry 3</li>
+                    <li>Entry 4</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
